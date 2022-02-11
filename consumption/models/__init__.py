f2366e5040d836515ffa6baece345c4df1324cdf
Mischback <dev@mischback.de>
2022-02-11 10:53:17 +0100

    Add models package

---
+++ consumption/models/__init__.py
@@ -0,0 +1,7 @@
+# SPDX-License-Identifier: MIT
+
+"""The app's models.
+
+This is provided as a package instead of a module, because the actual models
+are enhanced/augmented with model-related stuff like managers and model forms.
+"""
