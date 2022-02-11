80d5f761616a934562ae36310a82ec3b77d2555f
Mischback <dev@mischback.de>
2022-02-11 20:46:41 +0100

    Add ResourceDeleteView

--- consumption/views/resource.py
+++ consumption/views/resource.py
@@ -4,7 +4,7 @@
 
 # Django imports
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.urls import reverse_lazy  # noqa: F401
+from django.urls import reverse_lazy
 from django.views import generic
 
 # app imports
@@ -93,3 +93,42 @@ class ResourceUpdateView(LoginRequiredMixin, generic.UpdateView):
 
     template_name_suffix = "_update"
     """Uses the template ``templates/consumption/resource_update.html``."""
+
+
+class ResourceDeleteView(LoginRequiredMixin, generic.DeleteView):
+    """Generic class-based view to delete :class:`~consumption.models.resource.Resource` objects.
+
+    While this view requires a valid *login*, there is no check of permissions
+    (as of now), meaning: every (authenticated) user is able to delete any
+    :class:`~consumption.models.resource.Resource` object.
+
+    After successfully deleting an instance of
+    :class:`~consumption.models.resource.Resource` the user will be redirected
+    to the URL of :class:`~consumption.views.subject.SubjectDetailView` of the
+    :class:`~consumption.models.subject.Subject` instance the ``Resource`` was
+    associated with.
+
+    Uses the template ``templates/consumption/resource_confirm_delete.html``.
+    """
+
+    model = Resource
+    """Required attribute, determining the model to work on."""
+
+    context_object_name = "resource_instance"
+    """Provide a semantic name for the built-in context."""
+
+    pk_url_kwarg = "resource_id"
+    """The keyword argument as provided in :mod:`consumption.urls`."""
+
+    def get_success_url(self):
+        """Determine the URL for redirecting after successful deletion.
+
+        This has to be done dynamically with a method instead of statically
+        with the ``success_url`` attribute, because the user should be
+        redirected to the *parent* :class:`~consumption.models.subject.Subject`
+        instance.
+        """
+        subject = self.object.subject
+        return reverse_lazy(
+            "consumption:subject-detail", kwargs={"subject_id": subject.id}
+        )
