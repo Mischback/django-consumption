4b8e0c150132ccab1c378e2f1df9a130e0378240
Mischback <dev@mischback.de>
2022-02-11 16:07:47 +0100

    Add SubjectUpdateView

--- consumption/views/subject.py
+++ consumption/views/subject.py
@@ -69,3 +69,37 @@ class SubjectListView(generic.ListView):
 
     context_object_name = "subject_list"
     """Provide a semantic name for the built-in context."""
+
+
+class SubjectUpdateView(LoginRequiredMixin, generic.UpdateView):
+    """Generic class-based view to update :class:`~consumption.models.subject.Subject` objects.
+
+    While this view requires a valid *login*, there is no check of permissions
+    (as of now), meaning: every (authenticated) user is able to update any
+    :class:`~consumption.models.subject.Subject` object.
+
+    After successfully updating an instance of
+    :class:`~consumption.models.subject.Subject` the user will be redirected
+    to the URL as provided by
+    :meth:`Subject.get_absolute_url() <consumption.models.subject.Subject.get_absolute_url>`.
+    """
+
+    model = Subject
+    """Required attribute, determining the model to work on."""
+
+    form_class = SubjectForm
+    """Specify the form class to be used.
+
+    It's either the ``fields`` attribute or ``form_class``. This ultimatively
+    determines, which of the model's fields are rendered.
+
+    This app uses the ``form_class``, because it also overwrites the template
+    used for form rendering, see
+    :class:`~consumption.models.subject.SubjectForm` for details.
+    """
+
+    pk_url_kwarg = "subject_id"
+    """The keyword argument as provided in :source:`consumption.urls.py`."""
+
+    template_name_suffix = "_update"
+    """Uses the template ``templates/consumption/subject_update.html``."""
