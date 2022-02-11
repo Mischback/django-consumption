d8da54e83aa04b00e9d4b106cb58f35dde319628
Mischback <dev@mischback.de>
2022-02-11 15:13:15 +0100

    Provide URL for SubjectDetailView

--- consumption/urls.py
+++ consumption/urls.py
@@ -6,7 +6,7 @@
 from django.urls import path
 
 # app imports
-from consumption.views.subject import SubjectCreateView
+from consumption.views.subject import SubjectCreateView, SubjectDetailView
 
 app_name = "consumption"
 """Define an application namespace for reversing URLs.
@@ -16,4 +16,5 @@ See :djangodoc:`URL namespaces <topics/http/urls/#url-namespaces>`.
 
 urlpatterns = [
     path("subject/create/", SubjectCreateView.as_view(), name="subject-create"),
+    path("<int:subject_id>/", SubjectDetailView.as_view(), name="subject-detail"),
 ]
