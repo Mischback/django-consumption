6acc8f701b50f39786d932b86b3482c001edae02
Mischback <dev@mischback.de>
2022-02-11 12:13:11 +0100

    Fix errors from template setup
    - updated reference to the deprecated "dev_env"
    - fix debug_toolbar callback

--- tests/util/__init__.py
+++ tests/util/__init__.py
@@ -1,3 +1,11 @@
 # SPDX-License-Identifier: MIT
 
 """Utility stuff for running the test suite."""
+
+# Django imports
+from django.conf import settings
+
+
+def callback_show_debug_toolbar(request):
+    """Return ``settings.DEBUG`` to control debug_toolbar."""
+    return settings.DEBUG
