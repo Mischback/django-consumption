b2f0a2ef37bcdc8d4e23ab05f93dfd3460472a4b
Mischback <dev@mischback.de>
2022-02-11 10:12:16 +0100

    Replace 't3mpl4t3'  in app's urls.py

--- consumption/urls.py
+++ consumption/urls.py
@@ -5,7 +5,7 @@
 # Django imports
 from django.urls import path  # noqa: F401
 
-app_name = "t3mpl4t3"
+app_name = "consumption"
 """Define an application namespace for reversing URLs.
 
 See :djangodoc:`URL namespaces <topics/http/urls/#url-namespaces>`.
