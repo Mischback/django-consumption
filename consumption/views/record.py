5b43868977c5a06259633f86adcf859e10954d59
Mischback <dev@mischback.de>
2022-02-14 16:06:03 +0100

    Add RecordUpdateView

--- consumption/views/record.py
+++ consumption/views/record.py
@@ -55,3 +55,40 @@ class RecordDetailView(generic.DetailView):
 
     pk_url_kwarg = "record_id"
     """The keyword argument as provided in :mod:`consumption.urls`."""
+
+
+class RecordUpdateView(LoginRequiredMixin, generic.UpdateView):
+    """Generic class-based view to update :class:`~consumption.models.record.Record` objects.
+
+    While this view requires a valid *login*, there is no check of permissions
+    (as of now), meaning: every (authenticated) user is able to update any
+    :class:`~consumption.models.record.Record` object.
+
+    After successfully updating an instance of
+    :class:`~consumption.models.record.Record` the user will be redirected
+    to the URL as provided by
+    :meth:`Record.get_absolute_url() <consumption.models.record.Record.get_absolute_url>`.
+    """
+
+    model = Record
+    """Required attribute, determining the model to work on."""
+
+    form_class = RecordForm
+    """Specify the form class to be used.
+
+    It's either the ``fields`` attribute or ``form_class``. This ultimatively
+    determines, which of the model's fields are rendered.
+
+    This app uses the ``form_class``, because it also overwrites the template
+    used for form rendering, see
+    :class:`~consumption.models.record.RecordForm` for details.
+    """
+
+    context_object_name = "record_instance"
+    """Provide a semantic name for the built-in context."""
+
+    pk_url_kwarg = "record_id"
+    """The keyword argument as provided in :mod:`consumption.urls`."""
+
+    template_name_suffix = "_update"
+    """Uses the template ``templates/consumption/record_update.html``."""
